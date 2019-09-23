25419e24e1e1f7e505e49e4b50bebd7083ca672e
Roman Vovk <arromanvk@gmail.com>
2019-09-23 18:52:43 +0300

    Minor comment to config.js.

--- src/WebSpa/src/assets/js/config.js
+++ src/WebSpa/src/assets/js/config.js
@@ -3,6 +3,8 @@ var config = config || {};
   //
   // Set any global configs below using following format:
   //   context.myKey = "myValue";
+  // Then access it in any html or javascript file like this:
+  //   config.myKey
   //
   context.mgccUri = "https://localhost:44316";
 })(config);
