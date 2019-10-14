1117cda429f70dd6b531a4b09e54defa88fada8c
Roman Vovk <arromanvk@gmail.com>
2019-10-14 16:34:12 +0300

    Minor code cleanup.

--- src/Services/Operation/Controllers/MissilesController.cs
+++ src/Services/Operation/Controllers/MissilesController.cs
@@ -58,17 +58,5 @@ namespace Mlmc.Operation.Controllers
 
             return BadRequest();
         }
-
-        // // PUT api/values/5
-        // [HttpPut("{id}")]
-        // public void Put(int id, [FromBody] string value)
-        // {
-        // }
-
-        // // DELETE api/values/5
-        // [HttpDelete("{id}")]
-        // public void Delete(int id)
-        // {
-        // }
     }
 }
